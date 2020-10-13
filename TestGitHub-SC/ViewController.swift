1a734ff06563ce90027020fdb5bf7df2996a1f7c
sohail <sohail0411@gmail.com>
2020-10-13 11:13:28 +0530

    initial commit through SC changed colour of VC

--- TestGitHub-SC/ViewController.swift
+++ TestGitHub-SC/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        print("sohail shaik")
         // Do any additional setup after loading the view.
     }
 
+    
+    
 
 }
 
