c9c671e7e4a96ea0b936065342e52fce736f07be
sohail <sohail0411@gmail.com>
2020-10-13 11:05:22 +0530

    Initial Commit

---
+++ TestGitHub-SCTests/TestGitHub_SCTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestGitHub_SCTests.swift
+//  TestGitHub-SCTests
+//
+//  Created by Sohail Binanzala on 13/10/20.
+//  Copyright © 2020 Sohail Binanzala. All rights reserved.
+//
+
+import XCTest
+@testable import TestGitHub_SC
+
+class TestGitHub_SCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
